2a45d66a5a134cb380e2903d6e3edc2a56b5d116
18king <5659937-18king@users.noreply.replit.com>
2024-02-28 13:17:59 +0000

    mark ex1

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,11 @@
 # Exercise 1
-# Your solution comes here
+list = input("Enter a list of numbers: ")
+first_num = 0
+second_num = 0
+for i in range(5):
+  if i == 1 or i == 3:
+    second_num = second_num + int(list[i])
+  else:
+    first_num = first_num + int(list[i])
+final_num = str(first_num) + str(second_num)
+print(final_num)
