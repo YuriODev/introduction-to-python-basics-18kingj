4a724d57ff20a67df4f89d3002147f6bca585008
18kingj <161600931+18kingj@users.noreply.github.com>
2024-02-29 08:14:47 +0000

    3

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -3,4 +3,4 @@ seconds = int(input("Enter the time in seconds: "))
 hours = (seconds // 3600) % 24
 minutes = (seconds // 60) % 60
 seconds = seconds % 60
-print(f"{hours}:{minutes}:{seconds}")
+print(f"{hours}:{minutes:02d}:{seconds:02d}")
